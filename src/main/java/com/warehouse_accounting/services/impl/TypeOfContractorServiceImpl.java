c1f505cd07a7713518a9e494dbb5cbad50a722c0
ismaylov Nazir <Gympills@gmail.com>
2021-02-15 15:43:25 +0500

    add retrofit

--- src/main/java/com/warehouse_accounting/services/impl/TypeOfContractorServiceImpl.java
+++ src/main/java/com/warehouse_accounting/services/impl/TypeOfContractorServiceImpl.java
@@ -33,21 +33,46 @@ public class TypeOfContractorServiceImpl implements TypeOfContractorService {
 
     @Override
     public TypeOfContractorDto getById(Long id) {
-        return null;
+        try {
+            Call<TypeOfContractorDto> call = retrofit.getById(id);
+            Response<TypeOfContractorDto> response = call.execute();
+            return  response.body();
+        }catch (Exception e){
+            System.out.println(String.format("Ошибка при получении %s", id) + e);
+            throw new NotFoundException(e.toString());
+        }
     }
 
     @Override
     public void create(TypeOfContractorDto tcDTO) {
-
+        try {
+            Call<?> create = retrofit.create(tcDTO);
+            Response<?> response = create.execute();
+        }catch (Exception e){
+            System.out.println(String.format("Ошибка при создании %s", tcDTO.toString()) + e);
+            throw new NotFoundException(e.toString());
+        }
     }
 
     @Override
-    public void deleteByID(Long Id) {
-
+    public void deleteByID(Long id) {
+        try {
+            Call<?> delete = retrofit.delete(id);
+            Response<?> response = delete.execute();
+        }catch (Exception e){
+            System.out.println(String.format("Ошибка при получении %s", id) + e);
+            throw new NotFoundException(e.toString());
+        }
     }
 
     @Override
     public void update(TypeOfContractorDto tcDTO) {
-
+        try {
+            Call<?> update= retrofit.update(tcDTO);
+            Response<?> response = update.execute();
+        }catch (Exception e){
+            System.out.println(String.format("Ошибка при обновлении %s", tcDTO) + e);
+            throw new NotFoundException(e.toString());
+        }
     }
 }
