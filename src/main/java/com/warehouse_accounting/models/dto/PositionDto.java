f4dd89e1d3fb39f733593d4b2895a6ba01f25a93
VeraLimano <saydashevav@mail.ru>
2021-02-16 19:22:50 +0500

    position

---
+++ src/main/java/com/warehouse_accounting/models/dto/PositionDto.java
@@ -0,0 +1,20 @@
+package com.warehouse_accounting.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PositionDto {
+
+    Long id;
+
+    String name;
+
+    String sortNumber;
+
+}
