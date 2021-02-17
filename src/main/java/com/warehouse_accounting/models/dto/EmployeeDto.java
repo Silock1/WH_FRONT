38a4836343ac3dedad6e97c6ce7b4cf210042231
Akromo <zloekrevedko@yandex.ru>
2021-02-17 19:22:22 +0300

    + EmployeeService

---
+++ src/main/java/com/warehouse_accounting/models/dto/EmployeeDto.java
@@ -0,0 +1,43 @@
+package com.warehouse_accounting.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeDto {
+    private Long id;
+
+    private String lastName;
+
+    private String firstName;
+
+    private String middleName;
+
+    private String sortNumber;
+
+    private String phone;
+
+    private String inn;
+
+    private String description;
+
+    private String email;
+
+    private String password;
+
+    //private DepartmentDto department = new DepartmentDto();
+
+    private PositionDto position = new PositionDto();
+
+    private Set<RoleDto> roles;
+
+    private ImageDto image = new ImageDto();
+
+}
