924e5f3c2d3de85a2b69a34db70ffc955be246a8
Dima854 <dimonn104@gmail.com>
2021-02-19 16:54:31 +0500

    Создал contractor service

---
+++ src/main/java/com/warehouse_accounting/models/dto/ContractorDto.java
@@ -0,0 +1,44 @@
+package com.warehouse_accounting.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ContractorDto {
+
+    private Long id;
+
+    private String name;
+
+    private String inn;
+
+    private String sortNumber;
+
+    private String phone;
+
+    private String fax;
+
+    private String email;
+
+    private String address;
+
+    private String commentToAddress;
+
+    private String comment;
+
+    private ContractorGroupDto contractorGroup;
+
+    private TypeOfContractorDto typeOfContractor;
+
+    private TypeOfPriceDto typeOfPrice;
+
+    private List<BankAccountDto> bankAccounts;
+
+    private LegalDetailDto legalDetail;
+
+}
