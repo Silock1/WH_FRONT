7daa9e5c685289928df9324f1a6669b472bc2548
Akromo <zloekrevedko@yandex.ru>
2021-02-19 17:00:06 +0300

    + код стайл

--- src/main/java/com/warehouse_accounting/services/interfaces/EmployeeService.java
+++ src/main/java/com/warehouse_accounting/services/interfaces/EmployeeService.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public interface EmployeeService {
     List<EmployeeDto> getAll();
+
     EmployeeDto getById(Long id);
+
     void create(EmployeeDto employeeDto);
+
     void update(EmployeeDto employeeDto);
+
     void deleteById(Long id);
 }
