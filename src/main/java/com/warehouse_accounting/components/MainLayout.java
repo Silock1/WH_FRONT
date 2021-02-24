72e0d419a52ca6ae7777e594848121e054ded139
ismaylov Nazir <Gympills@gmail.com>
2021-02-24 16:07:36 +0500

    add retrofit

--- src/main/java/com/warehouse_accounting/components/MainLayout.java
+++ src/main/java/com/warehouse_accounting/components/MainLayout.java
@@ -4,14 +4,19 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
+import com.warehouse_accounting.model.dto.TypeOfContractorDto;
+import com.warehouse_accounting.services.impl.TypeOfContractorServiceImpl;
+import org.springframework.beans.factory.annotation.Value;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @Route(value = "main", layout = AppView.class)
 @RouteAlias(value = "", layout = AppView.class)
 @PageTitle("Главная | CRM")
-public class MainLayout extends VerticalLayout{
+public class MainLayout extends VerticalLayout {
 
     public MainLayout() {
 
-    }
 
+    }
 }
