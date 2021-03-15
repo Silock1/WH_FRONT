f7269b2c780666214225594194f0fe25d448a992
Aleksandr Aksenov <aks.aliaksandr@gmail.com>
2021-03-15 18:26:37 +0300

    added ProjectDto added ProjectServiceImpl added interface ProjectApi added interface ProjectService

---
+++ src/main/java/com/warehouse_accounting/services/interfaces/ProjectService.java
@@ -0,0 +1,17 @@
+package com.warehouse_accounting.services.interfaces;
+
+import com.warehouse_accounting.models.dto.ProjectDto;
+
+import java.util.List;
+
+public interface ProjectService {
+    List<ProjectDto> getAll();
+
+    ProjectDto getById(Long id);
+
+    void create(ProjectDto projectDto);
+
+    void update(ProjectDto projectDto);
+
+    void deleteById(Long id);
+}
