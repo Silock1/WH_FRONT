12fc4abcd7e0a7b67db1caebba236c3a75c38465
vetir@mail.ru <vetir@mail.ru>
2021-03-31 16:58:18 +0300

    Удалил закоментированный код.

--- src/main/java/com/warehouse_accounting/components/MoneySubMenuView.java
+++ src/main/java/com/warehouse_accounting/components/MoneySubMenuView.java
@@ -11,42 +11,6 @@ import com.vaadin.flow.router.Route;
 import java.util.Arrays;
 import java.util.List;
 
-//private final Div pageContent = new Div();
-//
-//public ProductionSubMenuView() {
-//        pageContent.setSizeFull();
-//        add(initSubMenu(), pageContent);
-//        }
-//
-//private Tabs initSubMenu() {
-//        List<String> goodsMenuItems = Arrays.asList("Тех. карты",
-//        "Заказы на производство",
-//        "Тех. операции");
-//        Tabs subMenuTabs = new Tabs();
-//        for (String item : goodsMenuItems) {
-//        Tab tab = new Tab();
-//        tab.addClassName("subMenuItem");
-//        tab.add(item);
-//        subMenuTabs.add(tab);
-//        }
-//        subMenuTabs.addSelectedChangeListener(event -> {
-//        switch (event.getSelectedTab().getLabel()) {
-//        case "Тех. карты":
-//        pageContent.removeAll();
-//        pageContent.add(new Span("Тех. карты"));
-//        break;
-//        case "Заказы на производство":
-//        pageContent.removeAll();
-//        pageContent.add(new Span("Заказы на производство"));
-//        break;
-//        case "Тех. операции":
-//        pageContent.removeAll();
-//        pageContent.add(new Span("Тех. операции"));
-//        break;
-//        }
-//        });
-//        return subMenuTabs;
-//        }
 @Route(value = "money", layout = AppView.class)
 @PageTitle("Деньги")
 public class MoneySubMenuView extends VerticalLayout {
