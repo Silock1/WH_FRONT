fb4cb249a6822d3c30e1879501e75b88610eeda1
Admi113 <nika1pb@mail.ru>
2021-04-18 19:35:51 +0300

    init addCallTable

--- src/main/java/com/warehouse_accounting/components/ContragentsSubMenuView.java
+++ src/main/java/com/warehouse_accounting/components/ContragentsSubMenuView.java
@@ -6,20 +6,25 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.warehouse_accounting.components.contractors.ContractorsOrders;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 import static com.warehouse_accounting.components.UtilView.subMenuTabs;
+
 
 @Route(value = "contragents", layout = AppView.class)
 @PageTitle("Контрагенты")
 public class ContragentsSubMenuView extends VerticalLayout {
 
     private final Div pageContent = new Div();
+    private ContractorsOrders contractorsOrders;
 
     public ContragentsSubMenuView() {
         pageContent.setSizeFull();
+        pageContent.add(initContractorOrders(pageContent));
         add(initSubMenu(), pageContent);
     }
 
@@ -41,11 +46,16 @@ public class ContragentsSubMenuView extends VerticalLayout {
                     break;
                 case "Звонки":
                     pageContent.removeAll();
-                    pageContent.add(new Span("Звонки"));
+                    pageContent.add(initContractorOrders(pageContent));
                     break;
             }
         });
         return subMenuTabs;
     }
-
+    private ContractorsOrders initContractorOrders(Div pageContent){
+        if (Objects.isNull(contractorsOrders)) {
+            contractorsOrders = new ContractorsOrders(pageContent);
+        }
+        return contractorsOrders;
+    }
 }
