4a2f5091b8d339c75a91d55cb67aa843da892d1f
Vaeltai <sisterofkelt@gmail.com>
2021-10-10 21:14:01 +0300

    AddAcceptancePage

---
+++ src/main/java/com/warehouse_accounting/components/purchases/Acceptances.java
@@ -0,0 +1,239 @@
+package com.warehouse_accounting.components.purchases;
+
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.contextmenu.MenuItem;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.menubar.MenuBar;
+import com.vaadin.flow.component.menubar.MenuBarVariant;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.textfield.TextFieldVariant;
+import com.warehouse_accounting.components.purchases.grids.AcceptancesGridLayout;
+
+public class Acceptances extends VerticalLayout {
+    private AcceptancesGridLayout acceptancesGridLayout;
+    private final TextField textField = new TextField();
+    private final Div parentLayer;
+
+    public Acceptances(Div parentLayer) {
+        this.parentLayer = parentLayer;
+        this.acceptancesGridLayout = new AcceptancesGridLayout(textField);
+        Div pageContent = new Div();
+        pageContent.add(acceptancesGridLayout);
+        pageContent.setSizeFull();
+        add(getGroupButtons(), pageContent);
+    }
+
+    private HorizontalLayout getGroupButtons() {
+        HorizontalLayout groupControl = new HorizontalLayout();
+
+        Button helpButton = new Button(new Icon(VaadinIcon.QUESTION_CIRCLE));
+        helpButton.addThemeVariants(ButtonVariant.LUMO_SMALL, ButtonVariant.LUMO_TERTIARY);
+        helpButton.addClickListener(e->{
+            Notification.show("Приемки позволяют учитывать закупки товаров. Приемку создают, когда покупают новый товар. Если товар уже лежит у вас на складе или вы не хотите указывать поставщиков, лучше воспользоваться оприходованием.\n" +
+                    "\n" +
+                    "В результате приемки увеличиваются остатки товаров в разделе Товары → Остатки и фиксируется долг перед поставщиком в разделе Деньги → Взаиморасчеты. Также на основе приемки формируется себестоимость товара.\n" +
+                    "\n" +
+                    "Приемку можно создать вручную или импортировать, в том числе из систем ЭДО.\n" +
+                    "\n" +
+                    "Читать инструкцию: Приемка товаров\n" +
+                    "\n" +
+                    "\n" +
+                    "Видео:\n" +
+                    "\n" +
+                    "    Приемка\n" +
+                    "    Цены в МоемСкладе\n" +
+                    "    Закупка и продажа товаров\n" +
+                    "\n", 5000, Notification.Position.TOP_START);
+        });
+
+        Label textProducts = new Label();
+        textProducts.setText("Приемки");
+
+        Button refreshButton = new Button(new Icon(VaadinIcon.REFRESH));
+        refreshButton.addThemeVariants(ButtonVariant.LUMO_SMALL, ButtonVariant.LUMO_TERTIARY);
+
+        Button addOrderButton = new Button("Приемка", new Icon(VaadinIcon.PLUS));
+        addOrderButton.addThemeVariants(ButtonVariant.LUMO_SMALL);
+
+        Button addFilterButton = new Button("Фильтр");
+        addFilterButton.addThemeVariants(ButtonVariant.LUMO_SMALL);
+
+        TextField searchField = new TextField();
+        searchField.setPlaceholder("Номер или комментарий");
+        searchField.addThemeVariants(TextFieldVariant.LUMO_SMALL);
+        searchField.addInputListener(inputEvent -> {
+
+        });
+
+        Button settingButton = new Button(new Icon(VaadinIcon.COG));
+        refreshButton.addThemeVariants(ButtonVariant.LUMO_SMALL, ButtonVariant.LUMO_TERTIARY);
+
+        HorizontalLayout editMenuBar = getEditMenuBar();
+        HorizontalLayout statusMenuBar = getStatusMenuBar();
+        HorizontalLayout createMenuBar = getCreateMenuBar();
+        HorizontalLayout printMenuBar = getPrintMenuBar();
+
+        groupControl.add(helpButton, textProducts, refreshButton, addOrderButton,
+                addFilterButton, searchField, editMenuBar, statusMenuBar, createMenuBar, printMenuBar, settingButton);
+        setSizeFull();
+        return groupControl;
+    }
+
+    private HorizontalLayout getEditMenuBar() {
+        Icon caretDownIcon = new Icon(VaadinIcon.CARET_DOWN);
+        caretDownIcon.setSize("12px");
+        textField.setReadOnly(true);
+        textField.addThemeVariants(TextFieldVariant.LUMO_SMALL);
+        textField.setWidth("30px");
+        textField.setValue("0");
+
+        MenuBar editMenuBar = new MenuBar();
+        editMenuBar.addThemeVariants(MenuBarVariant.LUMO_SMALL);
+        HorizontalLayout editItem = new HorizontalLayout(new Text("Изменить"), caretDownIcon);
+        editItem.setSpacing(false);
+        editItem.setAlignItems(Alignment.CENTER);
+
+        MenuItem editMenu = editMenuBar.addItem(editItem);
+        editMenu.getSubMenu().addItem("Удалить", menuItemClickEvent -> {
+            int selected = acceptancesGridLayout.getAcceptancesDtoGrid().asMultiSelect().getSelectedItems().size();
+            Notification notification = new Notification(String.format("Выделено для удаления %d", selected),
+                    3000, Notification.Position.MIDDLE);
+            notification.open();
+        });
+
+        editMenu.getSubMenu().addItem("Копировать", menuItemClickEvent -> {
+
+        });
+
+        editMenu.getSubMenu().addItem("Массовое редактирование", menuItemClickEvent -> {
+
+        });
+        editMenu.getSubMenu().addItem("Провести", menuItemClickEvent -> {
+
+        });
+        editMenu.getSubMenu().addItem("Снять проведение", menuItemClickEvent -> {
+
+        });
+        editMenu.getSubMenu().addItem("Объединить", menuItemClickEvent -> {
+
+        });
+
+
+        HorizontalLayout groupEdit = new HorizontalLayout();
+        groupEdit.add(textField, editMenuBar);
+        groupEdit.setSpacing(false);
+        groupEdit.setAlignItems(Alignment.CENTER);
+        return groupEdit;
+    }
+
+    private HorizontalLayout getStatusMenuBar() {
+        Icon caretDownIcon = new Icon(VaadinIcon.CARET_DOWN);
+        caretDownIcon.setSize("12px");
+        textField.setReadOnly(true);
+        textField.addThemeVariants(TextFieldVariant.LUMO_SMALL);
+        textField.setWidth("30px");
+        textField.setValue("0");
+
+        MenuBar statusMenuBar = new MenuBar();
+        statusMenuBar.addThemeVariants(MenuBarVariant.LUMO_SMALL);
+        HorizontalLayout horizontalLayout = new HorizontalLayout(new Text("Статус"), caretDownIcon);
+        horizontalLayout.setSpacing(false);
+        horizontalLayout.setAlignItems(Alignment.CENTER);
+
+        MenuItem statusItem = statusMenuBar.addItem(horizontalLayout);
+        statusItem.getSubMenu().addItem("Настроить...", e -> {
+
+        });
+        HorizontalLayout groupStatus = new HorizontalLayout();
+        groupStatus.add(statusMenuBar);
+        groupStatus.setSpacing(false);
+        groupStatus.setAlignItems(Alignment.CENTER);
+        return groupStatus;
+    }
+
+    private HorizontalLayout getCreateMenuBar() {
+        Icon caretDownIcon = new Icon(VaadinIcon.CARET_DOWN);
+        caretDownIcon.setSize("12px");
+        textField.setReadOnly(true);
+        textField.addThemeVariants(TextFieldVariant.LUMO_SMALL);
+        textField.setWidth("30px");
+        textField.setValue("0");
+
+        MenuBar createMenuBar = new MenuBar();
+        createMenuBar.addThemeVariants(MenuBarVariant.LUMO_SMALL);
+        HorizontalLayout horizontalLayout = new HorizontalLayout(new Text("Создать"), caretDownIcon);
+        horizontalLayout.setSpacing(false);
+        horizontalLayout.setAlignItems(Alignment.CENTER);
+
+        MenuItem createItem = createMenuBar.addItem(horizontalLayout);
+        createItem.getSubMenu().addItem("Счет поставщика", e -> {
+
+        });
+        createItem.getSubMenu().addItem("Счет-фактура полученный", e -> {
+
+        });
+
+        createItem.getSubMenu().addItem("Исходящий платеж", e -> {
+
+        });
+        createItem.getSubMenu().addItem("Расходный ордер", e -> {
+
+        });
+        createItem.getSubMenu().addItem("Возврат поставщику", e -> {
+
+        });
+        createItem.getSubMenu().addItem("Отмена", e -> {
+
+        });
+
+        HorizontalLayout groupCreate = new HorizontalLayout();
+        groupCreate.add(createMenuBar);
+        groupCreate.setSpacing(false);
+        groupCreate.setAlignItems(Alignment.CENTER);
+        return groupCreate;
+    }
+
+    private HorizontalLayout getPrintMenuBar() {
+        MenuBar printMenuBar = new MenuBar();
+        printMenuBar.addThemeVariants(MenuBarVariant.LUMO_SMALL);
+        Icon printIcon = new Icon(VaadinIcon.PRINT);
+        printIcon.setSize("12px");
+        Icon caretDownIcon = new Icon(VaadinIcon.CARET_DOWN);
+        caretDownIcon.setSize("12px");
+        HorizontalLayout printItem = new HorizontalLayout(printIcon, new Text(" Печать"), caretDownIcon);
+        printItem.setSpacing(false);
+        printItem.setAlignItems(Alignment.CENTER);
+        MenuItem print = printMenuBar.addItem(printItem);
+
+        print.getSubMenu().addItem("Список приемок", e -> {
+
+        });
+        print.getSubMenu().addItem("М-4", e -> {
+
+        });
+        print.getSubMenu().addItem("Приходная накладная", e -> {
+
+        });
+        print.getSubMenu().addItem("Комплект", e -> {
+
+        });
+        print.getSubMenu().addItem("Настроить...", e -> {
+
+        });
+
+        HorizontalLayout groupPrint = new HorizontalLayout();
+        groupPrint.add(printMenuBar);
+        groupPrint.setSpacing(false);
+        groupPrint.setAlignItems(Alignment.CENTER);
+        return groupPrint;
+    }
+
+}
