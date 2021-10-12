d558fb528d592966d8232715b2569576a9a407b6
Arslek <62049765+AlfViklund@users.noreply.github.com>
2021-10-12 18:37:19 +0500

    added Applications

---
+++ src/main/java/com/warehouse_accounting/services/interfaces/api/ApplicationApi.java
@@ -0,0 +1,37 @@
+package com.warehouse_accounting.services.interfaces.api;
+
+import com.warehouse_accounting.models.dto.ApplicationDto;
+import org.springframework.web.bind.annotation.PathVariable;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+import java.util.List;
+
+public interface ApplicationApi {
+
+    @Headers("Accept: application/json")
+    @GET("{url}")
+    Call<List<ApplicationDto>> getAll(@Path(value = "url", encoded = true) String url);
+
+    @Headers("Accept: application/json")
+    @GET("{url}/{id}")
+    Call<ApplicationDto> getById(@Path(value = "url", encoded = true) String url, @Path("id") Long id);
+
+    @Headers("Accept: application/json")
+    @POST("{url}")
+    Call<Void> create(@Path(value = "url", encoded = true) String url, @Body ApplicationDto applicationDto);
+
+    @Headers("Accept: application/json")
+    @PUT("{url}")
+    Call<Void> update(@Path(value = "url", encoded = true) String url, @Body ApplicationDto applicationDto);
+
+    @Headers("Accept: application/json")
+    @DELETE("{url}/{id}")
+    Call<Void> delete(@Path(value = "url", encoded = true) String url, @Path("id") Long id);
+}
