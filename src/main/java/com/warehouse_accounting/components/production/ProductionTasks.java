22c2adb928f2bf1f7b3b77c1daf57c914f74f5d9
MIhail <skuter63@mail.ru>
2022-01-21 16:39:01 +0400

    add button - edit, status, print

--- src/main/java/com/warehouse_accounting/components/production/ProductionTasks.java
+++ src/main/java/com/warehouse_accounting/components/production/ProductionTasks.java
@@ -3,16 +3,24 @@ package com.warehouse_accounting.components.production;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.contextmenu.MenuItem;
+import com.vaadin.flow.component.contextmenu.SubMenu;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.menubar.MenuBar;
+import com.vaadin.flow.component.menubar.MenuBarVariant;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.textfield.TextFieldVariant;
 import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
 import com.warehouse_accounting.components.production.grids.ProductionTasksFilterLayout;
 
+import javax.net.ssl.HostnameVerifier;
+import java.awt.*;
 
 
 @SpringComponent
@@ -59,7 +67,61 @@ public class ProductionTasks extends VerticalLayout {
             }
         });
 
-        horizontalLayout.add(helpButton, productionTasks, refreshButton, exercise, filter);
+        TextField textField = new TextField();
+        textField.setPlaceholder("Номер и комментарий");
+        textField.addThemeVariants(TextFieldVariant.LUMO_SMALL);
+        textField.setWidth("210px");
+
+        MenuBar menuBar = new MenuBar(); // Создаем меню бар
+        menuBar.addThemeVariants(MenuBarVariant.LUMO_ICON, MenuBarVariant.LUMO_CONTRAST);
+
+        TextField count = new TextField();
+        count.setValue("0");
+        count.setWidth("20px");
+        count.addThemeVariants(TextFieldVariant.LUMO_SMALL);
+
+        menuBar.addItem(count); // Добавим поле с кол-вом
+
+        Icon caretDownPrint = new Icon(VaadinIcon.CARET_DOWN);
+        caretDownPrint.setSize("13px");
+        Icon caretDownStatus = new Icon(VaadinIcon.CARET_DOWN);
+        caretDownStatus.setSize("13px");
+        Icon caretDownEdit = new Icon(VaadinIcon.CARET_DOWN);
+        caretDownEdit.setSize("13px");
+
+        HorizontalLayout editVision = new HorizontalLayout(new Text("Изменить"), caretDownEdit);
+        editVision.setSpacing(false);
+
+        MenuItem edit = menuBar.addItem(editVision);
+        SubMenu editSubMenu = edit.getSubMenu();
+        editSubMenu.addItem("Удалить").onEnabledStateChanged(false);
+        editSubMenu.addItem("Копировать").onEnabledStateChanged(false);
+        editSubMenu.addItem("Массовое редактирование");
+        editSubMenu.addItem("Провести").onEnabledStateChanged(false);
+        editSubMenu.addItem("Снять проведение").onEnabledStateChanged(false);
+
+        HorizontalLayout statusVision = new HorizontalLayout(new Text("Статус"), caretDownStatus);
+        statusVision.setSpacing(false);
+
+        MenuItem status = menuBar.addItem(statusVision);
+        SubMenu statusSubMenu = status.getSubMenu();
+        statusSubMenu.addItem("Настроить");
+
+        Icon printIcon = new Icon(VaadinIcon.PRINT);
+        printIcon.setSize("13px");
+        HorizontalLayout printVision = new HorizontalLayout(new Text("Печать"), printIcon, caretDownPrint);
+        statusVision.setSpacing(false);
+        MenuItem print = menuBar.addItem(printVision);
+        SubMenu printSubMenu = print.getSubMenu();
+        printSubMenu.addItem("Список производственных заданий");
+        printSubMenu.addItem("Производственное задание").onEnabledStateChanged(false);
+        printSubMenu.addItem("Комплект").onEnabledStateChanged(false);
+        printSubMenu.addItem("Настроить");
+
+
+        horizontalLayout.add(helpButton, productionTasks, refreshButton, exercise, filter, textField, menuBar
+                );
+
         return horizontalLayout;
     }
 
