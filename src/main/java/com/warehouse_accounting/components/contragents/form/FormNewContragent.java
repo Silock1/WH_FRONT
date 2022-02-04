7ac63b147aca9ee3014b808df11883236881ef40
MIhail <skuter63@mail.ru>
2022-02-04 20:36:19 +0400

    create form, add line

--- src/main/java/com/warehouse_accounting/components/contragents/form/FormNewContragent.java
+++ src/main/java/com/warehouse_accounting/components/contragents/form/FormNewContragent.java
@@ -3,6 +3,7 @@ package com.warehouse_accounting.components.contragents.form;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.contextmenu.MenuItem;
 import com.vaadin.flow.component.datepicker.DatePicker;
@@ -105,17 +106,33 @@ public class FormNewContragent extends VerticalLayout {
         formRequisites.addFormItem(paymentAccount, "Расчетный счет");
         formRequisites.setWidth("400px");
 
+        /*
+        Скидки и цены
+         */
         Text saleAndPrice = new Text("Скидки и цены");
         FormLayout formSalePrice = new FormLayout();
         ComboBox<String> price = new ComboBox<>();
         TextField discountCard = new TextField();
         formSalePrice.addFormItem(price, "Цены");
         formSalePrice.addFormItem(discountCard, "Дисконтная карта");
+//        formSalePrice.setResponsiveSteps(
+//                new FormLayout.ResponsiveStep("0", 1)
+//        );
+        formSalePrice.setWidth("400px");
 
         Text access = new Text("Доступ");
+        FormLayout accessForm = new FormLayout();
+        ComboBox<String> employee = new ComboBox<>();
+        ComboBox<String> departmentEmployee = new ComboBox<>();
+        Checkbox generalAccess = new Checkbox();
+
+        accessForm.addFormItem(employee, "Сотрудник");
+        accessForm.addFormItem(departmentEmployee,"Отдел");
+        accessForm.addFormItem(generalAccess, "Общий доступ");
+        accessForm.setWidth("400px");
 
         verticalLayout.add(textContragent, formContragent, textContactContragent, formLayout, textRequisites,
-                formRequisites, saleAndPrice,formSalePrice, access);
+                formRequisites, saleAndPrice,formSalePrice, access, accessForm);
 
 
         leftLayout.add(verticalLayout);
