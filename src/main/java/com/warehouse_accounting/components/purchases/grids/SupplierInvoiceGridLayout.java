1e2c43b874ba6d33adbe1c0a96fc93f7a7115842
Yuriy Vins <yuriy.vins83@gmail.com>
2022-02-06 21:02:23 +0600

    Создал класс EditInvoiceForm. Отредактировал класс InvoiceForm и переименовал на EditInvoiceForm. Отредактировал класс SupplierInvoiceGridLayout.
    Теперь документы можно изменять и удалять.

--- src/main/java/com/warehouse_accounting/components/purchases/grids/SupplierInvoiceGridLayout.java
+++ src/main/java/com/warehouse_accounting/components/purchases/grids/SupplierInvoiceGridLayout.java
@@ -1,16 +1,32 @@
 package com.warehouse_accounting.components.purchases.grids;
 
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.renderer.TemplateRenderer;
+import com.warehouse_accounting.components.purchases.forms.EditInvoiceForm;
 import com.warehouse_accounting.models.dto.SupplierInvoiceDto;
 import com.warehouse_accounting.services.impl.SupplierInvoiceServiceImpl;
 import com.warehouse_accounting.services.interfaces.SupplierInvoiceService;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 public class SupplierInvoiceGridLayout extends HorizontalLayout {
+
+    public static List<Long> gridEntityId = new ArrayList<>();
+    public static Div parentLayer;
+    public static Component returnLayer;
 
     public static Grid<SupplierInvoiceDto> initSupplierInvoiceGrid(){
 
@@ -26,9 +42,10 @@ public class SupplierInvoiceGridLayout extends HorizontalLayout {
                 .baseUrl("http://localhost:4446")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-
+        //---Добавить чекбокс в самый первый столбец.
         SupplierInvoiceService supplierInvoiceService = new SupplierInvoiceServiceImpl("/api/supplier_invoices",retrofit);
 
+        supplierInvoiceDtoGrid.setSelectionMode(Grid.SelectionMode.MULTI); //Добавляет чекбоксы в первый столбец.
         supplierInvoiceDtoGrid.addColumn(SupplierInvoiceDto::getId).setHeader("ID").setSortable(true).setAutoWidth(true);
         supplierInvoiceDtoGrid.addColumn(SupplierInvoiceDto::getInvoiceNumber).setHeader("Счет поставщика №").setSortable(true).setAutoWidth(true);
         supplierInvoiceDtoGrid.addColumn(SupplierInvoiceDto::getDateInvoiceNumber).setHeader("От даты").setSortable(true).setAutoWidth(true);
@@ -42,9 +59,74 @@ public class SupplierInvoiceGridLayout extends HorizontalLayout {
         supplierInvoiceDtoGrid.addColumn(SupplierInvoiceDto::getDateIncomingNumber).setHeader("От даты").setSortable(true).setAutoWidth(true);
         supplierInvoiceDtoGrid.addColumn(SupplierInvoiceDto::getAddPosition).setHeader("Наименование позиции").setSortable(true).setAutoWidth(true);
         supplierInvoiceDtoGrid.addColumn(SupplierInvoiceDto::getAddComment).setHeader("Комментарий").setSortable(true).setAutoWidth(true);
+        supplierInvoiceDtoGrid.addColumn(rowEdit(supplierInvoiceService)).setHeader("Изменить").setSortable(true).setAutoWidth(true);;
+        supplierInvoiceDtoGrid.addColumn(rowDelete(supplierInvoiceService)).setHeader("Удалить").setSortable(true).setAutoWidth(true);;
 
-        supplierInvoiceDtoGrid.setItems(supplierInvoiceService.getAll());
+        supplierInvoiceDtoGrid.setItems(supplierInvoiceService.getAll()); //Отображает данные на странице.
+
+        supplierInvoiceDtoGrid.setDetailsVisibleOnClick(false);
+        supplierInvoiceDtoGrid.setItemDetailsRenderer(rowEdit(supplierInvoiceService));
+        supplierInvoiceDtoGrid.setItemDetailsRenderer(rowDelete(supplierInvoiceService));
+
+        supplierInvoiceDtoGrid.addSelectionListener(selection -> { //Слушает чекбоксы.
+
+            Set<SupplierInvoiceDto> set = selection.getAllSelectedItems();
+
+            Iterator<SupplierInvoiceDto> iterator = set.iterator();
+
+            while(iterator.hasNext()){
+                gridEntityId.add(iterator.next().getId());
+            }
+        });
 
         return supplierInvoiceDtoGrid;
+    }
+
+    private static void editEndSave(SupplierInvoiceService supplierInvoiceService,SupplierInvoiceDto person){
+        EditInvoiceForm editInvoiceForm = new EditInvoiceForm(parentLayer, returnLayer, person.getId());
+        parentLayer.removeAll();
+        parentLayer.add(editInvoiceForm);
+    }
+
+    private static TemplateRenderer<SupplierInvoiceDto> rowEdit(SupplierInvoiceService supplierInvoiceService) {
+        return TemplateRenderer.<SupplierInvoiceDto>of(
+                        "<vaadin-button theme=\"tertiary\" on-click=\"handleClick\">Изменить</vaadin-button>")
+                .withEventHandler("handleClick", person ->
+                        editEndSave(supplierInvoiceService,person));
+    }
+    private static void deleteAndClose(SupplierInvoiceService supplierInvoiceService,SupplierInvoiceDto person){
+
+        Button delete = new Button("Удалить");
+        delete.addThemeVariants(ButtonVariant.LUMO_ERROR);
+        Button cancel = new Button("Отмена");
+        HorizontalLayout buttonLayout = new HorizontalLayout(cancel,delete);
+
+        Dialog dialog = new Dialog();
+        dialog.add("Подтвердите удаление");
+        dialog.add(new VerticalLayout());
+        dialog.add(buttonLayout);
+
+        dialog.open();
+
+        delete.addClickListener(event -> {
+            supplierInvoiceService.deleteById(person.getId());
+            gridEntityId.removeAll(gridEntityId);
+            parentLayer.removeAll();
+            parentLayer.add(returnLayer, SupplierInvoiceGridLayout.initSupplierInvoiceGrid());
+            dialog.close();
+        });
+
+        cancel.addClickListener(event -> {
+            dialog.close();
+        });
+    }
+
+    private static TemplateRenderer<SupplierInvoiceDto> rowDelete(SupplierInvoiceService supplierInvoiceService) {
+
+        return TemplateRenderer.<SupplierInvoiceDto>of(
+                        "<vaadin-button theme=\"tertiary\" on-click=\"handleClick\">Удалить</vaadin-button>")
+                .withEventHandler("handleClick", person ->
+                        deleteAndClose(supplierInvoiceService,person));
+
     }
 }
