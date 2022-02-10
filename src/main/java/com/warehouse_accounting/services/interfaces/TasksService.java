995c57ebb7d4ac9fddc39865b34573a325650be0
Anatolij Chernovol <anatolij-chernovol@yandex.ru>
2022-02-10 13:54:10 +0700

    git commit

---
+++ src/main/java/com/warehouse_accounting/services/interfaces/TasksService.java
@@ -0,0 +1,18 @@
+package com.warehouse_accounting.services.interfaces;
+
+import com.warehouse_accounting.models.dto.TasksDto;
+
+import java.util.List;
+
+public interface TasksService {
+
+    List<TasksDto> getAll();
+
+    TasksDto getById(Long id);
+
+    void create(TasksDto dto);
+
+    void update(TasksDto dto);
+
+    void deleteById(Long id);
+}
