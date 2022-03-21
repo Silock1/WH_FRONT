e52a69ff53644621fb072b41973861960f2b568b
Lina8388 <v.sem93@mail.ru>
2022-03-21 10:16:21 +0300

    2

--- src/main/java/com/warehouse_accounting/components/indiCators/grids/RecycleBinGridLayout.java
+++ src/main/java/com/warehouse_accounting/components/indiCators/grids/RecycleBinGridLayout.java
@@ -2,26 +2,36 @@ package com.warehouse_accounting.components.indiCators.grids;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.contextmenu.MenuItem;
 import com.vaadin.flow.component.contextmenu.SubMenu;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.menubar.MenuBar;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.component.textfield.NumberField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.StreamResource;
 import com.vaadin.flow.spring.annotation.UIScope;
 import com.warehouse_accounting.components.AppView;
+import com.warehouse_accounting.models.dto.PointOfSalesDto;
 import com.warehouse_accounting.models.dto.RecycleBinDto;
 import com.warehouse_accounting.services.interfaces.RecycleBinService;
 import org.springframework.stereotype.Component;
+import org.vaadin.olli.FileDownloadWrapper;
 
+
+import java.io.ByteArrayInputStream;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 
@@ -54,17 +64,18 @@ public class RecycleBinGridLayout extends VerticalLayout {
 
     private HashMap<String, String> getVisibleInvoiceColumn(){
         HashMap<String, String> fieldNameColumnName = new LinkedHashMap<>();
-        fieldNameColumnName.put("company.sortNumber","Тип документа");
-        fieldNameColumnName.put("name","№");
-        fieldNameColumnName.put("dateTime","Время");
+        fieldNameColumnName.put("documentType","Тип документа");
+        fieldNameColumnName.put("number","№");
+      //  fieldNameColumnName.put("date","Время");
         fieldNameColumnName.put("sum","Сумма");
-        fieldNameColumnName.put("warehouse.name","Со склада");
-        fieldNameColumnName.put("warehouse.sortNumber","На склад");
-        fieldNameColumnName.put("company.name","Организация");
-        fieldNameColumnName.put("contractor.name","Контрагент");
+        fieldNameColumnName.put("warehouse","Со склада");
+      //  fieldNameColumnName.put("warehouse","На склад");
+        fieldNameColumnName.put("companyName","Организация");
+        fieldNameColumnName.put("contractorName","Контрагент");
         fieldNameColumnName.put("status","Статус");
-        fieldNameColumnName.put("project.name","Отправлено");
-        fieldNameColumnName.put("project.description","Напечатоно");
+        fieldNameColumnName.put("project","Проект");
+        fieldNameColumnName.put("shipped","Отправлено");
+        fieldNameColumnName.put("printed","Напечатано");
         fieldNameColumnName.put("comment","Комментарий");
 
         return fieldNameColumnName;
@@ -126,17 +137,84 @@ public class RecycleBinGridLayout extends VerticalLayout {
             //TODO повод поработать этот функционал
         });
 
+        //модальное окно
+        Dialog dialog = new Dialog();
+        dialog.getElement().setAttribute("aria-label", "Создание печатной формы");
+        VerticalLayout dialogLayout = createDialogLayout(dialog);
+        dialog.add(dialogLayout);
+
         SubMenu printSubMenu = print.getSubMenu();
         MenuItem recycleBinList = printSubMenu.addItem("Список документов");
         recycleBinList.addClickListener(event -> {
+
+            dialog.open(); //
+
             //TODO повод поработать этот функционал
         });
 
         MenuItem configurePrint = printSubMenu.addItem("Настроить...");
         configurePrint.addClickListener(event -> {
+           // recycleBinService.getExcel();
             //TODO повод поработать этот функционал
         });
 
+        PointOfSalesDto pointOfSalesDto = new PointOfSalesDto();
+
+        //кнопка для скачивания, стоит отдельно
+        Button button = new Button("Click to download");
+        FileDownloadWrapper buttonWrapper = new FileDownloadWrapper(
+                new StreamResource("foo.txt", () -> new ByteArrayInputStream("hellou".getBytes())));
+        buttonWrapper.wrapComponent(button);
+        add(buttonWrapper);
+        TextField textField = new TextField("Enter file contents");
+        FileDownloadWrapper link = new FileDownloadWrapper("textfield.txt", () -> textField.getValue().getBytes());
+        link.setText("Download textfield.txt that has contents of the TextField");
+
+
+
         horizontalToolPanelLayout.add(helpButton,textProducts, refreshButton, addOrderButton, addFilterButton, searchField, numberField, menuBar);
+    }
+
+    //модальное окно
+    private static VerticalLayout createDialogLayout(Dialog dialog) {
+        H2 headline = new H2("Создание печатной формы");
+        headline.getStyle().set("margin", "var(--lumo-space-m) 0 0 0")
+                .set("font-size", "1.5em").set("font-weight", "bold");
+
+
+        Select<String> selectForm = new Select<String>("Открыть в браузере",
+                "Скачать в формате EXEL","Скачать в формате PDF", "Скачать в формате Open Office Calc");
+        Checkbox checkbox = new Checkbox("Запомнить выбор");
+        VerticalLayout fieldLayout = new VerticalLayout(selectForm,
+                checkbox);
+        fieldLayout.setSpacing(false);
+        fieldLayout.setPadding(false);
+        fieldLayout.setAlignItems(FlexComponent.Alignment.STRETCH);
+
+
+
+        Button cancelButton = new Button("Нет", e -> dialog.close());
+        Button saveButton = new Button("Да", e -> dialog.close());
+
+
+        FileDownloadWrapper buttonWrapper = new FileDownloadWrapper(
+                new StreamResource("привееет.txt", () -> new ByteArrayInputStream("привеееет".getBytes())));
+
+        buttonWrapper.wrapComponent(saveButton);
+        //add(buttonWrapper);
+
+        saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        HorizontalLayout buttonLayout = new HorizontalLayout(cancelButton,
+                buttonWrapper);
+        buttonLayout
+                .setJustifyContentMode(FlexComponent.JustifyContentMode.END);
+
+        VerticalLayout dialogLayout = new VerticalLayout(headline, fieldLayout,
+                buttonLayout);
+        dialogLayout.setPadding(false);
+        dialogLayout.setAlignItems(FlexComponent.Alignment.STRETCH);
+        dialogLayout.getStyle().set("width", "350px").set("max-width", "100%");
+
+        return dialogLayout;
     }
 }
--- src/main/java/com/warehouse_accounting/models/dto/RecycleBinDto.java
+++ src/main/java/com/warehouse_accounting/models/dto/RecycleBinDto.java
@@ -15,14 +15,47 @@ import java.time.LocalDateTime;
 public class RecycleBinDto {
 
     private Long id;
-    private String name;
-    private LocalDateTime dateTime;
+    private String documentType;
+    private String number;
+    private LocalDateTime date;
+    private BigDecimal sum  = BigDecimal.valueOf(0);
     private WarehouseDto warehouse = new WarehouseDto();
-    private CompanyDto company = new CompanyDto();
-    private BigDecimal sum;
-    private ContractorGroupDto contractor = new ContractorGroupDto();
-    private BigDecimal status;
+    private String warehouseFrom;
+    private CompanyDto companyName = new CompanyDto();
+    private ContractorGroupDto contractorName = new ContractorGroupDto();
+    private String status;
     private ProjectDto project = new ProjectDto();
-    private BigDecimal comment;
+    private String shipped;
+    private String printed;
+    private String comment;
 
+  /*  public RecycleBinDto(Long id,
+                         String documentType,
+                         String number,
+                         LocalDateTime date,
+                         BigDecimal sum,
+                         Long warehouseDto,
+                         String warehouseFrom,
+                         Long companyNameDto,
+                         Long contractorNameDto,
+                         String status,
+                         Long projectDto,
+                         String shipped,
+                         String printed,
+                         String comment) {
+        this.id = id;
+        this.documentType = documentType;
+        this.number = number;
+        this.date = date;
+        this.sum = sum;
+        this.warehouse.setId(warehouseDto);
+        this.warehouseFrom = warehouseFrom;
+        this.companyName.setId(companyNameDto);
+        this.contractorName.setId(contractorNameDto);
+        this.status = status;
+        this.project.setId(projectDto);
+        this.shipped = shipped;
+        this.printed = printed;
+        this.comment = comment;
+    }*/
 }
