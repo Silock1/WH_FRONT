3d0df42a6e9fbcf7b3dc925a2b26edc9e48f1c5d
Виктор <oscaruser91@mail.ru>
2022-05-13 18:20:52 +0300

    Добавление раздела Пользователь - Настройки - Проекты

--- src/main/java/com/warehouse_accounting/components/user/settings/projects/ProjectAddView.java
+++ src/main/java/com/warehouse_accounting/components/user/settings/projects/ProjectAddView.java
@@ -5,6 +5,8 @@ import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -14,8 +16,16 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.warehouse_accounting.components.user.settings.SettingsView;
 import com.warehouse_accounting.models.dto.ProjectDto;
+import com.warehouse_accounting.models.dto.TasksDto;
+import com.warehouse_accounting.services.impl.ProjectServiceImpl;
+import com.warehouse_accounting.services.impl.TasksServiceImpl;
 import com.warehouse_accounting.services.interfaces.ProjectService;
+import com.warehouse_accounting.services.interfaces.TasksService;
 import lombok.extern.log4j.Log4j2;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.time.format.DateTimeFormatter;
 
 
 @PageTitle("Добавить проект")
@@ -29,6 +39,7 @@ public class ProjectAddView extends VerticalLayout {
     private final Grid<ProjectDto> grid = new Grid<>();
     private Binder<ProjectDto> projectDtoBinder;
     private ProjectDto projectDto;
+    private Notification notification;
 
     public ProjectAddView(ProjectsView returnLayer
             ,ProjectService projectService) {
@@ -51,14 +62,25 @@ public class ProjectAddView extends VerticalLayout {
                 descriptionField);
 
         Button create = new Button("Сохранить", event -> {
-            try {
-                projectDtoBinder.writeBean(projectDto);
+//                Retrofit retrofit = new Retrofit.Builder()
+//                        .baseUrl("http://localhost:4446")
+//                        .addConverterFactory(GsonConverterFactory.create())
+//                        .build();
+
+//                ProjectService projectService = new ProjectService;
+//                        ("/api/project", retrofit);
+                ProjectDto projectDto = new ProjectDto();
+                projectDto.setName(nameField.getValue());
+                projectDto.setCode(codeField.getValue());
+                projectDto.setDescription(descriptionField.getValue());
                 projectService.create(projectDto);
-            } catch (ValidationException e) {
-                    log.error("Ошибка!");
-                };
-            parentLayer.removeAll();
-            parentLayer.add(returnLayer);
+
+//                log.error("Ошибка создания Проекта");
+//                notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
+//                notification.setPosition(Notification.Position.BOTTOM_STRETCH);
+
+//            parentLayer.removeAll();
+//            parentLayer.add(returnLayer);
         });
 
 
