6618ac204668c9c85022fef4356c029f26df5f87
Dmitriy <freejase@bk.ru>
2022-06-21 22:16:21 +0300

    Реализация Security front. Юзер/пароль - admin/admin

--- src/main/java/com/warehouse_accounting/WarehouseAccountingApplication.java
+++ src/main/java/com/warehouse_accounting/WarehouseAccountingApplication.java
@@ -2,6 +2,7 @@ package com.warehouse_accounting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication
 public class WarehouseAccountingApplication {
---
+++ src/main/java/com/warehouse_accounting/services/interfaces/UserService.java
@@ -0,0 +1,7 @@
+package com.warehouse_accounting.services.interfaces;
+
+import com.warehouse_accounting.models.dto.UserDto;
+
+public interface UserService {
+    void getAuthUser (UserDto userDto);
+}
