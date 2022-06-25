568423e7605cfeb723df47dfcc35d6a40807f8fb
rnozdrachev <roman.nozdracheff@yandex.ru>
2022-06-26 00:22:06 +0300

    добавил новые поля в ContractDto

--- src/main/java/com/warehouse_accounting/components/contragents/grids/ContractsGridLayot.java
+++ src/main/java/com/warehouse_accounting/components/contragents/grids/ContractsGridLayot.java
@@ -48,11 +48,21 @@ public class ContractsGridLayot extends HorizontalLayout {
         contractDtoGrid.addThemeVariants(GridVariant.LUMO_WRAP_CELL_CONTENT);
         contractDtoGrid.setHeightByRows(true);
 
-        Grid.Column<ContractDto> numberColumn = contractDtoGrid.addColumn(ContractDto::getNumber).setHeader("Номер").setSortable(true).setAutoWidth(true);
+        Grid.Column<ContractDto> numberColumn = contractDtoGrid.addColumn(ContractDto::getId).setHeader("Номер").setSortable(true).setAutoWidth(true);
+        Grid.Column<ContractDto> codColumn = contractDtoGrid.addColumn(ContractDto::getNumber).setHeader("Код").setSortable(true).setAutoWidth(true);
         Grid.Column<ContractDto> dateColumn = contractDtoGrid.addColumn(ContractDto::getContractDate).setHeader("Время").setSortable(true).setAutoWidth(true);
         Grid.Column<ContractDto> contractorNameColumn = contractDtoGrid.addColumn(contractDto -> contractDto.getContractorDto().getName()).setHeader("Контрагент").setSortable(true).setAutoWidth(true);
         Grid.Column<ContractDto> companyNameColumn = contractDtoGrid.addColumn(contractDto -> contractDto.getCompanyDto().getName()).setHeader("Организация").setSortable(true).setAutoWidth(true);
         Grid.Column<ContractDto> amountColumn = contractDtoGrid.addColumn(ContractDto::getAmount).setHeader("Сумма").setSortable(true).setAutoWidth(true);
+        Grid.Column<ContractDto> paidColumn = contractDtoGrid.addColumn(ContractDto::getPaid).setHeader("Оплачено").setSortable(true).setAutoWidth(true);
+        Grid.Column<ContractDto> doneColumn = contractDtoGrid.addColumn(ContractDto::getDone).setHeader("Выполнено").setSortable(true).setAutoWidth(true);
+        Grid.Column<ContractDto> isSharedAccessColumn = contractDtoGrid.addColumn(ContractDto::getIsSharedAccess).setHeader("Общий доступ").setSortable(true).setAutoWidth(true);
+        Grid.Column<ContractDto> ownerDepartmentColumn = contractDtoGrid.addColumn(contractDto -> contractDto.getOwnerDepartment() != null ? contractDto.getOwnerDepartment().getName() : null).setHeader("Владелец отдел").setSortable(true).setAutoWidth(true);
+        Grid.Column<ContractDto> ownerEmployeeColumn = contractDtoGrid.addColumn(contractDto -> contractDto.getOwnerEmployee() != null ? contractDto.getOwnerEmployee().getFirstName() : null).setHeader("Владелец сотрудник").setSortable(true).setAutoWidth(true);
+        Grid.Column<ContractDto> isSendColumn = contractDtoGrid.addColumn(ContractDto::getIsSend).setHeader("Отправлено").setSortable(true).setAutoWidth(true);
+        Grid.Column<ContractDto> isPrintColumn = contractDtoGrid.addColumn(ContractDto::getIsPrint).setHeader("Напечатано").setSortable(true).setAutoWidth(true);
+        Grid.Column<ContractDto> whenChangeColumn = contractDtoGrid.addColumn(ContractDto::getWhenChange).setHeader("Когда изменен").setSortable(true).setAutoWidth(true);
+        Grid.Column<ContractDto> whoChangeColumn = contractDtoGrid.addColumn(contractDto -> contractDto.getWhoChange() != null ? contractDto.getWhoChange().getFirstName() : null).setHeader("Кто изменил").setSortable(true).setAutoWidth(true);
         Grid.Column<ContractDto> commentColumn = contractDtoGrid.addColumn(ContractDto::getComment).setHeader("Комментарий").setSortable(true).setAutoWidth(true);
 
         contractDtoGrid.setSelectionMode(Grid.SelectionMode.MULTI);
@@ -60,11 +70,21 @@ public class ContractsGridLayot extends HorizontalLayout {
         settingButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
         ContractsGridLayot.ColumnToggleContextMenu columnToggleContextMenu = new ContractsGridLayot.ColumnToggleContextMenu(settingButton);
         columnToggleContextMenu.addColumnToggleItem("Номер", numberColumn);
+        columnToggleContextMenu.addColumnToggleItem("Код", codColumn);
         columnToggleContextMenu.addColumnToggleItem("Время", dateColumn);
         columnToggleContextMenu.addColumnToggleItem("Контрагент", contractorNameColumn);
         columnToggleContextMenu.addColumnToggleItem("Организация", companyNameColumn);
-        columnToggleContextMenu.addColumnToggleItem("Сумма", amountColumn);
+        columnToggleContextMenu.addColumnToggleItem("Сумма", paidColumn);
+        columnToggleContextMenu.addColumnToggleItem("Оплачено", doneColumn);
+        columnToggleContextMenu.addColumnToggleItem("Выполнено", amountColumn);
+        columnToggleContextMenu.addColumnToggleItem("Общий доступ", isSharedAccessColumn);
+        columnToggleContextMenu.addColumnToggleItem("Владелец отдел", ownerDepartmentColumn);
+        columnToggleContextMenu.addColumnToggleItem("Владелец сотрудник", ownerEmployeeColumn);
+        columnToggleContextMenu.addColumnToggleItem("Отправлено", isSendColumn);
+        columnToggleContextMenu.addColumnToggleItem("Напечатано", isPrintColumn);
         columnToggleContextMenu.addColumnToggleItem("Комментарий", commentColumn);
+        columnToggleContextMenu.addColumnToggleItem("Когда изменен", whenChangeColumn);
+        columnToggleContextMenu.addColumnToggleItem("Кто изменил", whoChangeColumn);
         return contractDtoGrid;
     }
 
