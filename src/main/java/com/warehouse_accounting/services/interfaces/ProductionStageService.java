5906aa0b892ba57b9d7f455df62fac4281e08453
Vladislav Chervinskii <niss.vlad@yandex.ru>
2022-09-04 09:26:06 +0300

    фикс

--- src/main/java/com/warehouse_accounting/services/interfaces/ProductionStageService.java
+++ src/main/java/com/warehouse_accounting/services/interfaces/ProductionStageService.java
@@ -5,6 +5,7 @@ import com.warehouse_accounting.models.dto.ProductionStageDto;
 import java.util.List;
 
 public interface ProductionStageService {
+
     List<ProductionStageDto> getAll();
 
     ProductionStageDto getById(Long id);
