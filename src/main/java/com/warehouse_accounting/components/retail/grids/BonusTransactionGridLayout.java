e2172e624950c1fb0beffaf0ae8ce62415159c89
crowl <silock314@gmail.com>
2022-10-08 22:33:35 +0300

    add Forms refactor: BonusTransaction decomposition

---
+++ src/main/java/com/warehouse_accounting/components/retail/grids/BonusTransactionGridLayout.java
@@ -0,0 +1,74 @@
+package com.warehouse_accounting.components.retail.grids;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.spring.annotation.SpringComponent;
+import com.warehouse_accounting.components.util.ColumnToggleContextMenu;
+import com.warehouse_accounting.components.util.SilverButton;
+
+import com.warehouse_accounting.models.dto.BonusTransactionDto;
+import com.warehouse_accounting.services.interfaces.BonusTransactionService;
+import lombok.Getter;
+import lombok.Setter;
+
+@SpringComponent
+@Getter
+@Setter
+public class BonusTransactionGridLayout extends HorizontalLayout {
+    private Grid<BonusTransactionDto> pointsGrid = new Grid<>(BonusTransactionDto.class, false);
+    private BonusTransactionService service;
+
+    public BonusTransactionGridLayout(BonusTransactionService service) {
+        this.service = service;
+
+        setSizeFull();
+        add(
+                tableContent()
+        );
+
+    }
+
+    private HorizontalLayout tableContent() {
+        HorizontalLayout horizontalLayout = new HorizontalLayout();
+
+        horizontalLayout.setSizeFull();
+        pointsGrid.addThemeVariants(GridVariant.LUMO_NO_BORDER);
+        pointsGrid.setItems(service.getAll());
+        pointsGrid.setSizeFull();
+        pointsGrid.setSelectionMode(Grid.SelectionMode.MULTI);
+
+
+        horizontalLayout.add(pointsGrid, settingButtonOperations());
+        return horizontalLayout;
+    }
+
+    private Button settingButtonOperations() {
+        SilverButton silverButton = new SilverButton();
+        Grid.Column<BonusTransactionDto> idColumn = pointsGrid.addColumn(BonusTransactionDto::getId).setHeader("№").setSortable(true);
+        Grid.Column<BonusTransactionDto> dateCreateColumn = pointsGrid.addColumn(BonusTransactionDto::getCreated).setHeader("Дата создания").setSortable(true);
+        Grid.Column<BonusTransactionDto> operationColumn = pointsGrid.addColumn(BonusTransactionDto::getTransactionType).setHeader("Тип операции").setSortable(true);
+        Grid.Column<BonusTransactionDto> bonusColumn = pointsGrid.addColumn(BonusTransactionDto::getBonusValue).setHeader("Бонусные баллы").setSortable(true);
+        Grid.Column<BonusTransactionDto> statusColumn = pointsGrid.addColumn(BonusTransactionDto::getTransactionStatus).setHeader("Статус").setSortable(true);
+        Grid.Column<BonusTransactionDto> dateChargeColumn = pointsGrid.addColumn(BonusTransactionDto::getExecutionDate).setHeader("Дата начисления").setSortable(true);
+        Grid.Column<BonusTransactionDto> bonusProgramColumn = pointsGrid.addColumn(transaction -> transaction.getBonusProgramDto().getName()).setHeader("Бонусная программа").setSortable(true);
+        Grid.Column<BonusTransactionDto> contragentColumn = pointsGrid.addColumn(transaction -> transaction.getContragent().getName()).setHeader("Контрагент").setSortable(true);
+        Grid.Column<BonusTransactionDto> commentaryColumn = pointsGrid.addColumn(BonusTransactionDto::getComment).setHeader("Комментарий").setSortable(true);
+
+        Button settingButton = silverButton.buttonSetting();
+        ColumnToggleContextMenu<BonusTransactionDto> columnToggleContextMenu = new ColumnToggleContextMenu<>(settingButton);
+        columnToggleContextMenu.addColumnToggleItem("№", idColumn);
+        columnToggleContextMenu.addColumnToggleItem("Дата создания", dateCreateColumn);
+        columnToggleContextMenu.addColumnToggleItem("Тип операции", operationColumn);
+        columnToggleContextMenu.addColumnToggleItem("Бонусные баллы", bonusColumn);
+        columnToggleContextMenu.addColumnToggleItem("Статус", statusColumn);
+        columnToggleContextMenu.addColumnToggleItem("Дата начисления", dateChargeColumn);
+        columnToggleContextMenu.addColumnToggleItem("Бонусная программа", bonusProgramColumn);
+        columnToggleContextMenu.addColumnToggleItem("Контрагент", contragentColumn);
+        columnToggleContextMenu.addColumnToggleItem("Комментарий", commentaryColumn);
+        return settingButton;
+    }
+
+
+}
---
+++ src/main/java/com/warehouse_accounting/components/retail/toolbars/BonusTransactionToolBar.java
@@ -0,0 +1,173 @@
+package com.warehouse_accounting.components.retail.toolbars;
+
+
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.contextmenu.MenuItem;
+import com.vaadin.flow.component.contextmenu.SubMenu;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Input;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.menubar.MenuBar;
+import com.vaadin.flow.component.menubar.MenuBarVariant;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.spring.annotation.SpringComponent;
+import com.warehouse_accounting.components.util.SilverButton;
+import com.warehouse_accounting.services.interfaces.BonusTransactionService;
+import lombok.Getter;
+import lombok.Setter;
+
+@SpringComponent
+@Getter
+@Setter
+public class BonusTransactionToolBar extends HorizontalLayout {
+    private MenuBar menuBar;
+    private BonusTransactionService service;
+    private Button refreshButton;
+
+    public BonusTransactionToolBar(BonusTransactionService service) {
+        this.service = service;
+
+        add(
+                toolBar()
+        );
+    }
+
+
+    private HorizontalLayout toolBar() {
+        HorizontalLayout toolbarLayout = new HorizontalLayout();
+        SilverButton silverButton = new SilverButton();
+        //toolbar settings
+        toolbarLayout.setWidthFull();
+        toolbarLayout.setAlignItems(Alignment.CENTER);
+
+        //MenuBar
+        MenuBar menuOperation = menuOperationButton();
+        //Filter
+        Button filter = silverButton.buttonBlank("Фильтр");
+        //Text
+        Span text = new Span("Операции с баллами");
+        text.getElement().getStyle().set("font-size", "20px");
+        text.getElement().getStyle().set("color", "#222");
+        text.getElement().getStyle().set("font-weight", "bold");
+
+
+        //Help
+        Button helpButton = silverButton.buttonHelp();
+
+        //Help realisation
+        helpButton.addClickListener(buttonClickEvent ->
+                show()
+        );
+
+        //TODO Refresh: getList from rest
+        refreshButton = silverButton.buttonRefresh();
+
+
+        //Inputs
+        Input searchField = new Input();
+        searchField.setPlaceholder("Номер или комментарий");
+        searchField.setWidth("200px");
+        Input miniField = new Input();
+        miniField.setWidth("25px");
+        miniField.setValue("0");
+
+        //MenuBar Edit
+        MenuBar edit = menuEditButton();
+
+
+        toolbarLayout.add(
+                helpButton,
+                text,
+                refreshButton,
+                menuOperation,
+                filter,
+                searchField,
+                miniField,
+                edit
+
+        );
+        return toolbarLayout;
+    }
+
+    private Notification show() {
+        Notification notification = new Notification();
+
+
+        Div text = new Div(helpNotificationText());
+
+        Button closeButton = new Button(new Icon("lumo", "cross"));
+        closeButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE);
+        closeButton.getElement().setAttribute("aria-label", "Close");
+        closeButton.addClickListener(event -> {
+            notification.close();
+        });
+
+        HorizontalLayout message = new HorizontalLayout(text, closeButton);
+        notification.add(message);
+        notification.open();
+        notification.setPosition(Notification.Position.TOP_START);
+
+        return notification;
+
+    }
+
+    private Text helpNotificationText() {
+
+        return new Text(
+                "Бонусные программы создаются в разделе Меню пользователя → Настройки → Скидки.\n" +
+                        "\n" +
+                        "Баллы начисляются и списываются из документа розничной продажи (возврата) или отдельно. Откройте розничную продажу или возврат, нажмите вверху на кнопку Создать документ и выберите в выпадающем списке Начисление баллов или Списание баллов.\n" +
+                        "\n" +
+                        "Баланс баллов отображается в карточке контрагента.\n" +
+                        "\n" +
+                        "Читать инструкцию: Бонусные программы"
+        );
+
+    }
+
+    private MenuBar menuOperationButton() {
+        menuBar = new MenuBar();
+        menuBar.addThemeVariants(MenuBarVariant.LUMO_ICON, MenuBarVariant.LUMO_CONTRAST);
+        menuBar.addItem(menuVision("Операция"));
+
+
+        return menuBar;
+    }
+
+    private MenuBar menuEditButton() {
+        MenuBar menuBar = new MenuBar();
+        menuBar.addThemeVariants(MenuBarVariant.LUMO_ICON, MenuBarVariant.LUMO_CONTRAST);
+
+        MenuItem operation = menuBar.addItem(menuVision("Изменить"));
+
+        SubMenu accureSubMenu = operation.getSubMenu();
+        accureSubMenu.addItem("Удалить")
+                .getElement()
+                .setAttribute("disabled", true);
+        accureSubMenu.addItem("Копировать")
+                .getElement()
+                .setAttribute("disabled", true);
+        accureSubMenu.addItem("Массовое редактирование");
+
+        return menuBar;
+    }
+
+    private HorizontalLayout menuVision(String textButton) {
+        Icon caretDownEdit = new Icon(VaadinIcon.CARET_DOWN);
+        caretDownEdit.setSize("13px");
+        Image image = new Image("icons/plusBlue.jpg", "Plus");
+
+        HorizontalLayout menuVision = new HorizontalLayout(image, new Span(textButton), caretDownEdit);
+        menuVision.setSpacing(false);
+        menuVision.getThemeList().add("spacing-s");
+        return menuVision;
+
+    }
+
+}
